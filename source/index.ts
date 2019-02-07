b8e769d3f20cb8f59308443adbfed5ff01b81545
Giorgos Charitakis <chgiorgos13@gmail.com>
2019-02-07 20:44:21 +0200

    Added extensions.json and settings.json. Changed prettier tabWidth to 4

--- source/index.ts
+++ source/index.ts
@@ -2,37 +2,33 @@ import * as bodyParser from "body-parser"
 import * as express from "express"
 
 class ResponseObject {
-  public id: string
-  public name: string
+    public id: string
+    public name: string
 
-  constructor(id: string, name: string) {
-    this.id = id
-    this.name = name
-  }
+    constructor(id: string, name: string) {
+        this.id = id
+        this.name = name
+    }
 }
 
 const app = express()
 app.set("port", 8080)
 app.use(bodyParser.json())
 app.post("/test", (request, response) => {
-  // tslint:disable-next-line:no-console
-  console.info(
-    `Received request \nheaders: ${JSON.stringify(request.headers, null, 3)} \nbody: ${JSON.stringify(
-      request.body,
-      null,
-      3
-    )}\n`
-  )
+    const headers = JSON.stringify(request.headers, null, 4)
+    const body = JSON.stringify(request.body, null, 4)
+    // tslint:disable-next-line:no-console
+    console.info(`Received request \nheaders: ${headers} \nbody: ${body}\n`)
 
-  const object = new ResponseObject("1", "some name")
-  response.set({
-    "Content-Type": "application/json",
-    "Custom-Header": "Some-custom-header-value"
-  })
-  // response.status(400)
-  response.json(object)
+    const object = new ResponseObject("1", "some name")
+    response.set({
+        "Content-Type": "application/json",
+        "Custom-Header": "Some-custom-header-value"
+    })
+    // response.status(400)
+    response.json(object)
 })
 app.listen(app.get("port"), () => {
-  // tslint:disable-next-line:no-console
-  console.info(`Started server at http://localhost:${app.get("port")}`)
+    // tslint:disable-next-line:no-console
+    console.info(`Started server at http://localhost:${app.get("port")}`)
 })
